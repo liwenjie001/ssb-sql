4287837cac0ab43e1ce6e844f46e86e797023ab5
liwenjie001 <1554616416@qq.com>
2023-02-03 11:28:05 +0800

    Create hive多表查询SQL.sql

---
+++ hive多表查询SQL.sql
@@ -0,0 +1,134 @@
+--Q1.1
+select sum(lo_revenue) as revenue
+from lineorder join dates on lo_orderdate = d_datekey
+where d_year = 1993 and lo_discount between 1 and 3 and lo_quantity < 25;
+ 
+--Q1.2
+select sum(lo_revenue) as revenue
+from lineorder
+join dates on lo_orderdate = d_datekey
+where d_yearmonthnum = 199401
+and lo_discount between 4 and 6
+and lo_quantity between 26 and 35;
+ 
+--Q1.3
+select sum(lo_revenue) as revenue
+from lineorder
+join dates on lo_orderdate = d_datekey
+where d_weeknuminyear = 6 and d_year = 1994
+and lo_discount between 5 and 7
+and lo_quantity between 26 and 35;
+ 
+ 
+--Q2.1
+select sum(lo_revenue) as lo_revenue, d_year, p_brand
+from lineorder
+inner join dates on lo_orderdate = d_datekey
+join part on lo_partkey = p_partkey
+join supplier on lo_suppkey = s_suppkey
+where p_category = 'MFGR#12' and s_region = 'AMERICA'
+group by d_year, p_brand
+order by d_year, p_brand;
+ 
+--Q2.2
+select sum(lo_revenue) as lo_revenue, d_year, p_brand
+from lineorder
+join dates on lo_orderdate = d_datekey
+join part on lo_partkey = p_partkey
+join supplier on lo_suppkey = s_suppkey
+where p_brand between 'MFGR#2221' and 'MFGR#2228' and s_region = 'ASIA'
+group by d_year, p_brand
+order by d_year, p_brand;
+ 
+--Q2.3
+select sum(lo_revenue) as lo_revenue, d_year, p_brand
+from lineorder
+join dates on lo_orderdate = d_datekey
+join part on lo_partkey = p_partkey
+join supplier on lo_suppkey = s_suppkey
+where p_brand = 'MFGR#2239' and s_region = 'EUROPE'
+group by d_year, p_brand
+order by d_year, p_brand;
+ 
+ 
+--Q3.1
+select c_nation, s_nation, d_year, sum(lo_revenue) as lo_revenue
+from lineorder
+join dates on lo_orderdate = d_datekey
+join customer on lo_custkey = c_custkey
+join supplier on lo_suppkey = s_suppkey
+where c_region = 'ASIA' and s_region = 'ASIA'and d_year >= 1992 and d_year <= 1997
+group by c_nation, s_nation, d_year
+order by d_year asc, lo_revenue desc;
+ 
+--Q3.2
+select c_city, s_city, d_year, sum(lo_revenue) as lo_revenue
+from lineorder
+join dates on lo_orderdate = d_datekey
+join customer on lo_custkey = c_custkey
+join supplier on lo_suppkey = s_suppkey
+where c_nation = 'UNITED STATES' and s_nation = 'UNITED STATES'
+and d_year >= 1992 and d_year <= 1997
+group by c_city, s_city, d_year
+order by d_year asc, lo_revenue desc;
+ 
+--Q3.3
+select c_city, s_city, d_year, sum(lo_revenue) as lo_revenue
+from lineorder
+join dates on lo_orderdate = d_datekey
+join customer on lo_custkey = c_custkey
+join supplier on lo_suppkey = s_suppkey
+where (c_city='UNITED KI1' or c_city='UNITED KI5')
+and (s_city='UNITED KI1' or s_city='UNITED KI5')
+and d_year >= 1992 and d_year <= 1997
+group by c_city, s_city, d_year
+order by d_year asc, lo_revenue desc;
+ 
+--Q3.4
+select c_city, s_city, d_year, sum(lo_revenue) as lo_revenue
+from lineorder
+join dates on lo_orderdate = d_datekey
+join customer on lo_custkey = c_custkey
+join supplier on lo_suppkey = s_suppkey
+where (c_city='UNITED KI1' or c_city='UNITED KI5') and (s_city='UNITED KI1' or s_city='UNITED KI5') and d_yearmonth
+ = 'Dec1997'
+group by c_city, s_city, d_year
+order by d_year asc, lo_revenue desc;
+ 
+ 
+--Q4.1
+select d_year, c_nation, sum(lo_revenue) - sum(lo_supplycost) as profit
+from lineorder
+join dates on lo_orderdate = d_datekey
+join customer on lo_custkey = c_custkey
+join supplier on lo_suppkey = s_suppkey
+join part on lo_partkey = p_partkey
+where c_region = 'AMERICA' and s_region = 'AMERICA' and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2')
+group by d_year, c_nation
+order by d_year, c_nation;
+ 
+--Q4.2
+select d_year, s_nation, p_category, sum(lo_revenue) - sum(lo_supplycost) as profit
+from lineorder
+join dates on lo_orderdate = d_datekey
+join customer on lo_custkey = c_custkey
+join supplier on lo_suppkey = s_suppkey
+join part on lo_partkey = p_partkey
+where c_region = 'AMERICA'and s_region = 'AMERICA'
+and (d_year = 1997 or d_year = 1998)
+and (p_mfgr = 'MFGR#1' or p_mfgr = 'MFGR#2')
+group by d_year, s_nation, p_category
+order by d_year, s_nation, p_category;
+ 
+--Q4.3
+select d_year, s_city, p_brand, sum(lo_revenue) - sum(lo_supplycost) as profit
+from lineorder
+join dates on lo_orderdate = d_datekey
+join customer on lo_custkey = c_custkey
+join supplier on lo_suppkey = s_suppkey
+join part on lo_partkey = p_partkey
+where c_region = 'AMERICA'and s_nation = 'UNITED STATES'
+and (d_year = 1997 or d_year = 1998)
+and p_category = 'MFGR#14'
+group by d_year, s_city, p_brand
+order by d_year, s_city, p_brand;
